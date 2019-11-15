25b63328b2077f44e84580fc92d0a4a0e29a393a
Katie Wright <katie.wrght@gmail.com>
2019-11-15 14:05:45 +0100

    handle change of player on componentWillUpdate

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,15 +6,25 @@ class Game extends Component {
     super(props)
     this.state = {
       board: new Array(9).fill(''),
-      player1: 'X',
+      currentPlayer: true,
     };
   }
 
-  handleClick = (e, cell, idx) => {
-    if(cell.length === 0) {
-      this.setState((state) => state.board[idx] = 'X')
+  componentDidUpdate(prevProps, prevState) {
+    console.log('update')
+    if(this.state.currentPlayer === prevState.currentPlayer) {
+      this.setState({ currentPlayer: !prevState.currentPlayer });
     }
   };
+
+  handleClick = (e, cell, idx) => {
+    if(cell.length === 0) {
+      this.setState((state) =>
+      state.board[idx] = this.state.currentPlayer ? 'X' : 'O'
+      );
+    }
+  };
+
 
   render() {
     const { board } = this.state;
