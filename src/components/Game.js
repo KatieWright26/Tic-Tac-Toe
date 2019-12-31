140d7255c61bbcfad3c85ee4bdfda3017ca53cff
Katie Wright <katie.wrght@gmail.com>
2019-12-31 15:49:04 +0100

    move winning moves into own folder

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,18 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Board } from './Board';
 import { Scoreboard } from './Scoreboard';
 import { WinnerAlert } from './WinnerAlert';
-
-const POSSIBLE_WINS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
-
+import { POSSIBLE_WINS } from '../lib/winningMoves';
 
 function Game() {
   const [board, setBoard] = useState(new Array(9).fill(''));
